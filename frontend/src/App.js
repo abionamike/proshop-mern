6da502e9eebc2a2110be897b21e6b1fd5e273ade
Abiona Michael <amabiona21@gmail.com>
2020-10-03 22:15:13 +0100

    react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/navbar';
+import BookList from './components/bookList';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
